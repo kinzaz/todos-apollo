27a8454aa36f36ea0e3e7a57a1f56dbdfb0c7f19
Vladislav Vlasenko <kinzazdev579@gmail.com>
2023-05-01 17:45:26 +0300

    todo ready

---
+++ src/apollo/todos.js
@@ -0,0 +1,43 @@
+import { gql } from '@apollo/client';
+
+export const ALL_TODO = gql`
+	query AllTodos {
+		todos: allTodos {
+			id
+			title
+			completed
+		}
+	}
+`;
+
+export const ADD_TODO = gql`
+	mutation addTodo($title: String!, $userId: ID!, $completed: Boolean!) {
+		newTodo: createTodo(
+			title: $title
+			user_id: $userId
+			completed: $completed
+		) {
+			id
+			title
+			completed
+		}
+	}
+`;
+
+export const UPDATE_TODO = gql`
+	mutation updateTodo($id: ID!, $completed: Boolean) {
+		updateTodo(id: $id, completed: $completed) {
+			id
+			title
+			completed
+		}
+	}
+`;
+
+export const DELETE_TODO = gql`
+	mutation DeleteTodo($id: ID!) {
+		removeTodo(id: $id) {
+			id
+		}
+	}
+`;
